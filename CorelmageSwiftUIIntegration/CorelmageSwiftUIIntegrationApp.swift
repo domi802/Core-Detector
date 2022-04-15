48c21f3f2322696f30aa171991d05cc7d4301e4c
domi802 <64065747+domi802@users.noreply.github.com>
2022-04-15 16:16:02 +0200

    Initial Commit

---
+++ CorelmageSwiftUIIntegration/CorelmageSwiftUIIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CorelmageSwiftUIIntegrationApp.swift
+//  CorelmageSwiftUIIntegration
+//
+//  Created by Dominik Woźniak on 15/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CorelmageSwiftUIIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
